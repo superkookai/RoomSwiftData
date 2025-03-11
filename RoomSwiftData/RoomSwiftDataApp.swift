84134fbbd9d716f5a11df41cfb0cb888f27dd3b1
superkookai <weerawut@live.com>
2025-03-11 11:22:35 +0700

    Initial Commit

---
+++ RoomSwiftData/RoomSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomSwiftDataApp.swift
+//  RoomSwiftData
+//
+//  Created by Weerawut Chaiyasomboon on 11/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct RoomSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
